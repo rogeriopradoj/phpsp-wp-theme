ab2648657a8c3accb2240dd00b8192531e85fe80
William Espindola <bkpserver@gmail.com>
2013-03-02 18:09:45 -0300

    Criando a header do site

--- header.php
+++ header.php
@@ -25,77 +25,30 @@
 		<link href="<?php echo esc_url( home_url( '/' ) ); ?>wp-content/themes/phpsp-wp-theme/css/phpsp.css" rel="stylesheet" type="text/css"/>
 	</head>
 	
-	<body <?php body_class(); ?>>
-		<div class="container">
-			<div id="page" class="hfeed row">
-				<?php tha_header_before(); ?>
-				<header id="branding" role="banner" class="span12">
-					<?php tha_header_top();
-					wp_nav_menu( array(
-						'container'			=>	'nav',
-						'container_class'	=>	'subnav clearfix',
-						'theme_location'	=>	'header-menu',
-						'menu_class'		=>	'nav nav-pills pull-right',
-						'depth'				=>	3,
-						'fallback_cb'		=>	false,
-						'walker'			=>	new The_Bootstrap_Nav_Walker,
-					) ); ?>
-					<hgroup>
-						<h1 id="site-title">
-							<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-								<span><?php bloginfo( 'name' ); ?></span>
-							</a>
-						</h1>
-						<h2 id="site-description"><?php bloginfo( 'description' ); ?></h2>
-					</hgroup>
-					
-					<?php if ( get_header_image() ) : ?>
-					<a id="header-image" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-						<img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="" />
-					</a>
-					<?php endif; // if ( get_header_image() ) ?>
+    <body <?php body_class(); ?>>
+        <header class="header-container">
+            <section class="header-section wrapper clearfix">
+              <h1 class="title"><img src="img/logo.png" alt=""></h1>
+              <div class="blogingo">Grupo de Desenvolvedores PHP de São Paulo</div>
+              <form class="form-search" action="" method="post" accept-charset="utf-8">
+                <input type="text" name="searc" value="" placeholder="Buscar no PHPSP">
+                <button type="button">OK</button>
+              </form>
+            </section>
+            <nav class="clearfix nav">
+              <div class="clearfix nav-topo">
+                <ul id="menu-principal" class="wrapper menu">
+                  <li id="menu-item-367" class="menu-item current-menu-item current_page_item menu-item-367">
+                    <a title="Página inicial" href="http://infosign.net.br">Artigos</a>
+                  </li>
+                  <li id="menu-item-868" class="menu-item menu-item-868">
+                    <a href="http://infosign.net.br/topico/envelopamento-de-carros/">Envelopamento</a>
+                  </li>
+                </ul>                
+              </div>
+            </nav>
+        </header>
 
-					<nav id="access" role="navigation">
-						<h3 class="assistive-text"><?php _e( 'Main menu', 'the-bootstrap' ); ?></h3>
-						<div class="skip-link"><a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to primary content', 'the-bootstrap' ); ?>"><?php _e( 'Skip to primary content', 'the-bootstrap' ); ?></a></div>
-						<div class="skip-link"><a class="assistive-text" href="#secondary" title="<?php esc_attr_e( 'Skip to secondary content', 'the-bootstrap' ); ?>"><?php _e( 'Skip to secondary content', 'the-bootstrap' ); ?></a></div>
-						<?php if ( has_nav_menu( 'primary' ) OR the_bootstrap_options()->navbar_site_name OR the_bootstrap_options()->navbar_searchform ) : ?>
-						<div <?php the_bootstrap_navbar_class(); ?>>
-							<div class="navbar-inner">
-								<div class="container">
-									<!-- .btn-navbar is used as the toggle for collapsed navbar content -->
-									<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-										<span class="icon-bar"></span>
-										<span class="icon-bar"></span>
-										<span class="icon-bar"></span>
-									</a>
-									<?php if ( the_bootstrap_options()->navbar_site_name ) : ?>
-									<span class="brand"><?php bloginfo( 'name' ); ?></span>
-									<?php endif;?>
-									<div class="nav-collapse">
-										<?php wp_nav_menu( array(
-											'theme_location'	=>	'primary',
-											'menu_class'		=>	'nav',
-											'depth'				=>	3,
-											'fallback_cb'		=>	false,
-											'walker'			=>	new The_Bootstrap_Nav_Walker,
-										) ); 
-										if ( the_bootstrap_options()->navbar_searchform ) {
-											the_bootstrap_navbar_searchform();
-										} ?>
-								    </div>
-								</div>
-							</div>
-						</div>
-						<?php endif; ?>
-					</nav><!-- #access -->
-					<?php if ( function_exists( 'yoast_breadcrumb' ) ) {
-						yoast_breadcrumb( '<nav id="breadcrumb" class="breadcrumb">', '</nav>' );
-					}
-					tha_header_bottom(); ?>
-				</header><!-- #branding --><?php
-				tha_header_after();
-				
-
+<?php
 /* End of file header.php */
 /* Location: ./wp-content/themes/the-bootstrap/header.php */
