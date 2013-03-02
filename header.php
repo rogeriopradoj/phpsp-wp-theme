431ce52b6af8575a5e08b5d1e2e3f9b75f9c62d4
Anderson Casimiro <o@duodra.co>
2013-03-02 12:23:25 -0300

    updated webfont setting

--- header.php
+++ header.php
@@ -18,7 +18,8 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		
 		<title><?php wp_title( '&laquo;', true, 'right' ); ?></title>
-		
+		<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,400italic,700italic&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+
 		<?php tha_head_bottom(); ?>
 		<?php wp_head(); ?>
 	</head>
