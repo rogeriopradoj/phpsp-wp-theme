58d4037477c4761aa3d2cb580c3c6c157ed73cc6
Augusto Pascutti <augusto@phpsp.org.br>
2013-03-02 19:00:19 -0300

    New footer page without right align.
    Conflicts:
    	css/phpsp.css

--- footer.php
+++ footer.php
@@ -5,35 +5,42 @@
  * @package		The Bootstrap
  * @since		1.0.0	- 05.02.2012
  */
+				if (!function_exists('add_action')) {
+				    die('<!-- erro -->');
+				}
 
-				tha_footer_before(); ?>
-				<footer id="colophon" role="contentinfo" class="span12">
-					<?php tha_footer_top(); ?>
-					<div id="page-footer" class="well clearfix">
-						<?php wp_nav_menu( array(
-							'container'			=>	'nav',
-							'container_class'	=>	'subnav',
-							'theme_location'	=>	'footer-menu',
-							'menu_class'		=>	'credits nav nav-pills pull-left',
-							'depth'				=>	3,
-							'fallback_cb'		=>	'the_bootstrap_credits',
-							'walker'			=>	new The_Bootstrap_Nav_Walker,
-						) );
-						?>
-						<div id="site-generator"<?php echo has_nav_menu('footer-menu') ? ' class="footer-nav-menu"' : ''; ?>>
-							<a	href="<?php echo esc_url( __( 'http://wordpress.org/', 'the-bootstrap' ) ); ?>"
-								title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'the-bootstrap' ); ?>"
-								target="_blank"
-								rel="generator"><?php printf( _x( 'Proudly powered by %s', 'WordPress', 'the-bootstrap' ), 'WordPress' ); ?></a>
-						</div>
-					</div><!-- #page-footer .well .clearfix -->
-					<?php tha_footer_bottom(); ?>
-				</footer><!-- #colophon -->
+                tha_footer_before(); ?>
+                <footer id="colophon" role="contentinfo" class="span12">
+                	<div id="footerx">
+                        <div id="facebook-box">
+                        	<div class="fb-like-box" data-href="http://www.facebook.com/sao.paulo.elephants" data-width="900" data-show-faces="true" data-stream="false" data-header="false" >
+                        	</div> 
+                        </div>
+                        <br class="clear" />
+                        <div class="blocker-sponsor">
+	                        <div id="sponsor-box">
+	                            <ul>
+	                                <li><a class="" href="#" title="UOL Host" target="_blank"><img src="<?php echo esc_url(home_url( '/' )) ?>wp-content/themes/phpsp-wp-theme/img/sponsor_00.png"/></a></li>
+	                                <li><a class="" href="#" title="PHP" target="_blank"><img src="<?php echo esc_url(home_url( '/' )) ?>wp-content/themes/phpsp-wp-theme/img/sponsor_01.png" /></a></li>
+	                                <li><a class="" href="#" title="MS Windows Server 2008" target="_blank"><img src="<?php echo esc_url(home_url( '/' )) ?>wp-content/themes/phpsp-wp-theme/img/sponsor_02.png" /></a></li>
+	                                <li><a class="" href="#" title="IIS" target="_blank"><img src="<?php echo esc_url(home_url( '/' )) ?>wp-content/themes/phpsp-wp-theme/img/sponsor_03.png" /></a></li>
+	                                <li><a class="" href="#" title="iMasters" target="_blank"><img src="<?php echo esc_url(home_url( '/' )) ?>wp-content/themes/phpsp-wp-theme/img/sponsor_04.png" /></a></li>
+	                            </ul>
+	                        </div>
+	                    </div>
+                        <br class="clear" />
+                        <div id="creative-commons">
+                          	<a class="img-cc" rel="license" href="http://creativecommons.org/licenses/by-nc-nd/3.0/deed.pt">
+                            	<img alt="Licença Creative Commons" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-nd/3.0/88x31.png" />
+                            </a>
+                            <p class="desc-cc">O trabalho <span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">Portal</span> de <a xmlns:cc="http://creativecommons.org/ns#" href="http://phpsp.org.br" property="cc:attributionName" rel="cc:attributionURL">PHP-SP</a> foi licenciado com uma Licença <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/3.0/deed.pt">Creative Commons - Atribuição-NãoComercial-SemDerivados 3.0 Não Adaptada</a>.<br />Com base no trabalho disponível em <a xmlns:dct="http://purl.org/dc/terms/" href="https://github.com/PHPSP/phpsp-wp-theme" rel="dct:source">https://github.com/PHPSP/phpsp-wp-theme</a>.</p>
+                        </div>
+                    </div>
+				</footer>
 				<?php tha_footer_after(); ?>
-			</div><!-- #page -->
-		</div><!-- .container -->
+			</div>
+		</div>
 	<!-- <?php printf( __( '%d queries. %s seconds.', 'the-bootstrap' ), get_num_queries(), timer_stop(0, 3) ); ?> -->
-	<?php wp_footer(); ?>
 	</body>
 </html>
 <?php
